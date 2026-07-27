a2874603b6216002f432ba172014972cf63e354d
Namdinh13 <abct53085@gmail.com>
2026-07-27 13:43:45 +0700

    Design A Level

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -5,6 +5,8 @@ public class Sheep : MonoBehaviour
 {
     [SerializeField] private GameObject sheepPrefabs;
     [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private float spawnRangeX = 10f;
+
     private float timer;
 
     void Update()
@@ -13,7 +15,10 @@ public class Sheep : MonoBehaviour
 
         if (timer > spawnInterval)
         {
-            Instantiate(sheepPrefabs, transform.position, sheepPrefabs.transform.rotation);
+            float xPos = UnityEngine.Random.Range(-spawnRangeX, spawnRangeX);
+            Vector3 spawnPosition = new Vector3(xPos, transform.position.y, transform.position.z);
+
+            Instantiate(sheepPrefabs, spawnPosition, sheepPrefabs.transform.rotation);
             timer = 0f;
         }
     }
